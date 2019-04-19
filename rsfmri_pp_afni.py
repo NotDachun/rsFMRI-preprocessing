f5712eb12a9f1e7fed84f000a7adbfb9f844f6d3
Daniel Zhu <dzhu99@uw.edu>
2019-04-19 15:43:16 -0700

    Fixed bugs, added functionality for -r flag and creation of EM snapshot

--- rsfmri_pp_afni.py
+++ rsfmri_pp_afni.py
@@ -13,6 +13,13 @@ Rationale:
     -Easier to maintain/change in the future
     -Better for larger scale programs
     -Ability to do unit testing
+
+ToDo:
+	-Test parse_args
+	-Test override ability
+	-Move files to correct folder
+	-Test EPI on MNI snapshot
+	-Create dynamic DMN QC
 """
 
 import argparse
@@ -40,6 +47,20 @@ def record(text):
 	if output_file:
 		output_file.write(str(text) + "\n")
 
+def isclose(a, b, rel_tol=1e-09, abs_tol=0.0):
+	"""
+	Checks if two floats are equal
+
+	Parameters:
+		a (float/double): float to be checked for equivalence
+		b (float/double): float be to checked for equivalence
+		rel_tol (float/double): relative tolerance 
+		abs_tol (float/double): absolute tolerance
+	Returns:
+		True if floats are equal, otherwise false
+	"""
+	return abs(a-b) <= max(rel_tol * max(abs(a), abs(b)), abs_tol)
+
 def parse_args(args):
 	"""
 	Creates a parser and parses arguments.
@@ -66,7 +87,8 @@ def parse_args(args):
 	parser.add_argument("-b", "--bandpass", action="store", default=[0.01, 0.25], nargs=2, type=float, metavar="F",
 	                    help="bandpass frequencies limits (default = 0.01 0.25)")
 	parser.add_argument("-v", "--volumes", action="store", default=0, type=int, metavar="V",
-						help="truncate the epi data to the inputted number of volumes, useful if subjects have data with different numbers of volumes (default = no truncation)")
+						help="""truncate the epi data to the inputted number of volumes, useful if subjects have data 
+						with different numbers of volumes (default = no truncation)""")
 	parser.add_argument("-f", "--fwhm", action="store", default=5.0, type=float, metavar="MM",
 						help="the full width half maximum that is used when blurring (default = 5.0 mm)")
 	parser.add_argument("-c", "--cores", action="store", default=cpu_count(), type=int, metavar="C",
@@ -102,11 +124,11 @@ def parse_args(args):
 	assert (os.path.isfile(result.epi)), "{} does not exist or is not a file".format(result.epi)
 	assert (os.path.isfile(result.anat)), "{} does not exist or is not a file".format(result.ant)
 	assert (result.trs_remove >= 0), "Cannot remove negative trs"
-	assert (args.dim_voxel >= 0), "Cannot have a negative voxel dimension"
-	assert (not np.all(np.array(result.bandpass) > 0)), "Cannot have a negative frequency limit for bandpassing"
-	assert (result.volumes > 0), "Number of volumes must be greater than 0"
+	assert (result.dim_voxel >= 0), "Cannot have a negative voxel dimension"
+	assert (np.all(np.array(result.bandpass) > 0)), "Cannot have a negative frequency limit for bandpassing"
+	assert (result.volumes > -1), "Number of volumes must be greater than 0"
 	assert (result.cores > 0), "Number of cores used must be greater than 0"
-	assert (result.bandpass_ts > 0), "Time step must be greater than 0"
+	assert (result.bandpass_ts > -1), "Time step must be greater than 0"
 
 	return result
 
@@ -144,7 +166,7 @@ def run_SSwarper(out_dir, subj_id, anat, template):
 	assert (os.path.isfile(anat)), "Path to anatomical data does not exist"
 	assert (os.path.isfile(template)), "Path to template does not exist"
 
-	sswarper_outdir = out_dir + "SSwarper_Output/"
+	sswarper_outdir = out_dir + ("/" if out_dir[-1] != "/" else "") + "SSwarper_Output/"
 
     # Check if desired output exists already
 	if (os.path.isdir(sswarper_outdir) and
@@ -189,18 +211,24 @@ def truncate_epi(epi, out_dir, volumes):
 	# Equivalent to shell command: 3dinfo -nv $epi
 	subj_vol = int(subprocess.check_output([
 		"3dinfo", "-nv", epi]))
+	record("The current number of volumes is {}".format(subj_vol))
 
 	if (subj_vol > volumes):
-		record("Truncating to first {} volumnes".format(volumes))
-		epi_to990 = out_dir + "to990"
+		record("Truncating to first {} volumes".format(volumes))
+		epi_toVolumes = out_dir + "to{}".format(volumes)
 
 		# Use afni's 3dTcat to truncate epi to inputted number of volumes
 		# Equivalent to shell command: 3dTcat -prefix "${outdir}/to990" $epi'[0..989]
 		record(subprocess.check_output([
-			"3dTcat", "-prefix", epi_to990,
-			"{}'[0..989]'".format(epi)], stderr=subprocess.STDOUT))
-		epi_to990 += "+orig."
-		return epi_to990
+			"3dTcat -prefix {} {}'[0..{}]'".format(epi_toVolumes, epi, volumes - 1)], 
+			shell=True, stderr=subprocess.STDOUT))
+		epi_toVolumes += "+orig."
+		epi = epi_toVolumes
+
+	check_vol = int(subprocess.check_output([
+		"3dinfo", "-nv", epi]))
+	assert (check_vol == volumes), "TCat EPI does not have the correct number of volumes"
+
 	return epi
 
 def generate_afni_proc(args, template):
@@ -214,7 +242,7 @@ def generate_afni_proc(args, template):
 		afni_proc.py script in list format separated by spaces
 	"""
 
-	sswarper_outdir = args.out_dir + "SSwarper_Output/"
+	sswarper_outdir = args.out_dir + ("/" if args.out_dir[-1] != "/" else "") + "SSwarper_Output/"
 
 	# Create afni_proc.py shell command
 	record("Generating afni_proc.py script")
@@ -225,30 +253,30 @@ def generate_afni_proc(args, template):
 	"-blocks", "despike", "align", "tlrc", "volreg", "mask", "blur", "scale", "regress",
 	"-copy_anat", sswarper_outdir + "anatSS.{}.nii".format(args.subj_id),
 	"-anat_has_skull", "no",
-	"dsets", args.epi,
-	"-tcat_remove_first_trs", args.trs_remove,
+	"-dsets", args.epi,
+	"-tcat_remove_first_trs", str(args.trs_remove),
 	"-align_opts_aea", "-giant_move", "-cost", "lpc+zz",
 	"-volreg_align_to", "MIN_OUTLIER",
 	"-volreg_align_e2a",
 	"-volreg_tlrc_warp",
 	"-tlrc_base", template,
-	"-volreg_warp_dxyz", args.dim_voxel]
+	"-volreg_warp_dxyz", str(args.dim_voxel)]
 
 	# Pass SSwarper output to afni_proc.py
 	if args.nl_reg:
 		afni_proc.extend([
 			"-tlrc_NL_warp",
 			"-tlrc_NL_warped_dsets",
-			sswarper_outdir + "anatQQ.{}.nii".format(subj_id),
-			sswarper_outdir + "anatQQ.{}.aff12.1D".format(subj_id),
-			sswarper_outdir + "anatQQ.{}_WARP.nii".format(subj_id)])
+			sswarper_outdir + "anatQQ.{}.nii".format(args.subj_id),
+			sswarper_outdir + "anatQQ.{}.aff12.1D".format(args.subj_id),
+			sswarper_outdir + "anatQQ.{}_WARP.nii".format(args.subj_id)])
 
 	afni_proc.extend([
-		"-blur_size", "5.0",
+		"-blur_size", str(args.fwhm),
 		"-mask_segment_anat", "yes",
 		"-mask_segment_erode", "yes",
 		"-regress_motion_per_run",
-		"-regress_bandpass", args.bandpass[0], args.bandpass[1],
+		"-regress_bandpass", str(args.bandpass[0]), str(args.bandpass[1]),
 		"-regress_apply_mot_types", "demean"])
 
 	# Regress 12 motion parameters
@@ -262,11 +290,10 @@ def generate_afni_proc(args, template):
 
 	afni_proc.extend([
 		"-regress_opts_3dD",
-		"-jobs", args.cores,
-		"-regress_run_clustersim", "no",
-		"-regress_est_blur_epits",
-		"-regress_est_blur_errts"])
+		"-jobs", str(args.cores),
+		"-regress_run_clustersim", "no"])
 
+	assert (all(isinstance(param, str) for param in afni_proc)), "Not all parameters are strings"
 	return afni_proc
 
 def set_epi_tr(epi, tr):
@@ -280,20 +307,32 @@ def set_epi_tr(epi, tr):
 
 	record("Ensuring TR in EPI header info is correct.")
 	# Equivalent to shell command: 3dinfo -tr $epi
-	curr_tr = subprocess.check_output([
-		"3dinfo", "-tr", epi])
-	record("The current TR is {}", curr_tr)
+	curr_tr = float(subprocess.check_output(["3dinfo", "-tr", epi]))
+	record("The current TR is {}".format(curr_tr))
 
-	if (not np.isclose(curr_tr, tr)):
-		record("Modifying TR from {} to be {}", curr_tr, tr)
+	if (not isclose(curr_tr, tr)):
+		record("Modifying TR from {} to be {}".format(curr_tr, tr))
 		# Equivalent to shell command: 3drefit -TR $epi
-		record(subprocess.check_output([
-			"3drefit", "-TR", tr, epi]))
+		record(subprocess.check_output(["3drefit", "-TR", str(tr), epi]))
 
-	check_tr = subprocess.check_output([
-		"3dinfo", "-tr", epi])
+	check_tr = float(subprocess.check_output(["3dinfo", "-tr", epi]))
 
-	assert (np.isclose(check_tr, tr)), "TR was not modified correctly"
+	assert (isclose(check_tr, tr)), "TR was not modified correctly"
+
+def create_EM_snapshot(final_epi, subj_id, template):
+	"""
+	Creates a snapshot to check for alignment between the processed epi and
+	an inputted template
+
+	Parameters:
+		final_epi (String): Path to the processed epi
+		subj_id (String): Subject id
+		template (String): Path to the template that was used for registration
+	"""
+	# Equivalent to shell command: @snapshot_volreg $template $subj.results/pb03.$subj.r01.volreg+tlrc. EM_$subj.jpg
+	# Names snapshot EM_$subj.jpg
+	record(subprocess.check_output([
+		"@snapshot_volreg", template, final_epi, "EM_{}.jpg"format(subj_id)]))
 
 def clean(subj_id):
 	"""
@@ -302,36 +341,53 @@ def clean(subj_id):
 	Parameter:
 		subj_id (String): Subject Id
 	"""
-	
-	if (os.path.isdir("{}.results".format(subj_id))):
-		shutil.rmtree("{}.results".format(subj_id))
+	old_dir = "{}.results".format(subj_id)
+
+	if (os.path.isdir(old_dir)):
+		record("Clean - Removing {}".format(old_dir))
+		shutil.rmtree(old_dir)
 
 def run():
 	"""
 	Runs this script
 
 	"""
-
 	mni = "MNI152_T1_2mm_SSW.nii.gz"
 
 	args = parse_args(sys.argv[1:])
 
 	clean(args.subj_id)
 
-	args.out_dir = create_outdir(args.out_dir)
-	if args.nl_reg:
-		run_SSwarper(args.out_dir, args.subj_id, args.anat, mni)
-	if args.volumes:
-		args.epi = truncate_epi()
-	if args.bandpass_ts:
-		set_epi_tr(args.epi, args.bandpass_ts)
+	os.environ['OMP_NUM_THREADS'] = args.cores
+	final_result = args.out_dir + create_outdir(args.out_dir)
 
-	afni_proc = generate_afni_proc(args, mni)
-	# Execute afni_proc.py, creates RSproc.$subj script
-	record(subprocess.check_output(afni_proc, stderr=subprocess.STDOUT))
+	result = "{}.results/errts.sub.tproject+tlrc".format(args.subj_id)
+	if (os.path.isfile(final_result + ".HEAD") and os.path.isfile(final_result + ".BRIK")
+		and not args.rerun):
+		record("Data has already been preprocessed.")
+		record("Results of preprocessing are the following files: ")
+		record(final_result + ".HEAD")
+		record(final_result + ".BRIK")
+		record("Use flag -r/--rerun to override results and rerun the preprocessing")
+		return
+	
+	else:
+		shutil.rmtree(args.out_dir)
+		os.mkdir(args.out_dir)
 
-	# Execute RSproc.$subj
-	record(subprocess.check_output(["tcsh", "-xef", "RSproc.{}".format(args.subj_id)]))
+		if args.nl_reg:
+			run_SSwarper(args.out_dir, args.subj_id, args.anat, mni)
+		if args.volumes:
+			args.epi = truncate_epi()
+		if args.bandpass_ts:
+			set_epi_tr(args.epi, args.bandpass_ts)
+
+		afni_proc = generate_afni_proc(args, mni)
+		# Execute afni_proc.py, creates RSproc.$subj script
+		record(subprocess.check_output(afni_proc, stderr=subprocess.STDOUT))
+
+		# Execute RSproc.$subj
+		record(subprocess.check_output(["tcsh", "-xef", "RSproc.{}".format(args.subj_id)]))
 
 if __name__ == "__main__":
 	run()
