b9f7936adbcd191870cc3375cece9218b29f766c
Daniel Zhu <dzhu99@uw.edu>
2019-04-24 10:03:44 -0700

    QC and Seed Mask

---
+++ dynamic_connectivity.py
@@ -0,0 +1,238 @@
+#!/usr/bin/env python3
+# Displays the dynamic connectivity for an input seed mask and data set
+
+import numpy as np
+import nibabel as nib
+import argparse
+import shutil
+import os
+from collections import defaultdict
+from timeit import default_timer as timer
+
+# credit to Berkeley Psych 214: https://github.com/psych-214-fall-2016/classwork/blob/master/pearson/solution/pearson.py
+def pearson_2d(x, Y):
+    """ Pearson product-moment correlation of vectors `x` and array `Y`
+    Parameters
+    ----------
+    x : array shape (N,)
+        One-dimensional array to correlate with every column of `Y`
+    Y : array shape (N, P)
+        2D array where we correlate each column of `Y` with `x`.
+    Returns
+    -------
+    r_xy : array shape (P,)
+        Pearson product-moment correlation of vectors `x` and the columns of
+        `Y`, with one correlation value for every column of `Y`.
+    """
+    # Mean-center x -> mc_x
+    # Mean-center every column of Y -> mc_Y
+    # a : Get sum of products of mc_x and every column of mc_Y
+    # b : Get sum of products of mc_x on mc_x
+    # c : Get sum of products of every column of mc_Y[:, i] on itself
+    # return a / (sqrt(b) * sqrt(c))
+    mc_x = x - np.mean(x)
+    mc_Y = Y - np.mean(Y, axis=0)  # This is numpy broadcasting
+    # You could also do the step above with:
+    # mc_Y = Y - np.tile(np.mean(Y, axis=0), (len(x), 1))
+    a = mc_x.dot(mc_Y)
+    b = mc_x.dot(mc_x)
+    c = np.sum(mc_Y ** 2, axis=0)
+    return a / (np.sqrt(b) * np.sqrt(c))
+
+'''
+Param:
+    data = path to processed epi
+    roi = path to roi mask
+Behavior: 
+    Returns a np array for subject and seed and original epi data affine
+'''
+def read_data(data, roi):
+    print("Reading images and loading data")
+    subject = read_subject(data)
+    seed = read_roi(roi)
+    affine = get_affine(data)
+    return subject, seed, affine
+
+'''
+Param:
+    roi = path to roi mask
+Behavior: 
+    Returns a np array for inputted roi
+'''
+def read_roi(roi):
+    seed = nib.load(roi, mmap=False)
+    return seed.get_data()
+
+'''
+Param:
+    data = path to processed epi
+Behavior: 
+    Returns a np array for inputted data
+'''
+def read_subject(data):
+    subject = nib.load(data, mmap=False)
+    return subject.get_data()
+
+'''
+Param:
+    data = path to processed epi
+Behavior: 
+    Returns the data's affine
+'''
+def get_affine(img):
+    img = nib.load(img, mmap=False)
+    return img.affine
+
+'''
+Param:
+    roi_list = text file of roi paths
+Behavior: 
+    Returns a dictionary with file name as key and np array of roi as value
+'''
+def read_all_roi(roi_list):
+    rois = dict();
+    with open(roi_list) as roi_paths:
+        for roi in roi_paths:
+            name = os.path.basename(roi[:-1])
+            rois[name[:name.find(".")]] = read_roi(roi.strip())
+    return rois
+
+'''
+Param:
+    subject_list = text file of subject paths
+Behavior: 
+    Returns a list of np arrays for each subject
+'''
+def read_all_subject(subject_list):
+    subjects = []
+    with open(subject_list) as subs:
+       for subject in subs:
+           subjects.append(read_subject(subject.strip()))
+    return subjects
+
+'''
+Param:
+   subjects = list of np arrays for each subject
+   rois = dictionary of roi name and np array for each roi
+Behavior: 
+   Returns a dictionary with keys as roi names and values as a list
+   of means of every subject's time course extracted from the roi
+'''
+def calc_mean(subjects, rois):
+    roi_means = defaultdict(list)
+    for name, roi in rois.items():
+        nans = 0
+        for subject in subjects:
+            roi_tc = extract_seed_tc(roi, subject)
+            mean = np.nanmean(roi_tc)
+            if (not np.isnan(mean)):
+                roi_means[name].append(mean)
+            else:
+                nans += 1
+        if nans:
+            print("Removed " + str(nans) + " values from " + name + "'s means")
+    return roi_means
+
+'''
+Param:
+    subjects = list of np arrays for each subject
+    rois = dictionary of roi name and np array for each roi
+Behavior: 
+    Returns a dictionary with keys as roi names and values as a list
+    of standard deviations of every subject's time course extracted from the roi
+'''
+def calc_std(subjects, rois):
+    roi_means = defaultdict(list)
+    for name, roi in rois.items():
+        nans = 0
+        for subject in subjects:
+            roi_tc = extract_seed_tc(roi, subject)
+            std = np.nanstd(roi_tc)
+            if (not np.isnan(std)):
+                roi_means[name].append(std)
+            else:
+                nans += 1
+        if nans:
+            print("Removed " + str(nans) + " values from " + name + "'s stds")
+    return roi_means
+
+'''
+Param:
+    window_length = window length in trs
+    overlap = overlap in trs
+    volumes = total number of volumes
+Behavior: 
+    Returns the number of windows
+'''
+def calc_windows(window_length, overlap, volumes):
+    return (volumes - window_length) // (window_length - overlap) + 1
+
+# Extract time course of seed region
+def extract_seed_tc(mask, data):
+    print("Extracting the time course of seed region")
+    if (mask.shape != data.shape[:-1]):
+        print("WARNING: Make sure mask has same dimensions as data!")
+    masked_data = np.array([data[index] for index in zip(*np.where(mask > 0))])
+    if (np.isnan(masked_data).any()):
+        print ("WARNING: There are NaN values within the mask")
+    return np.nanmean(masked_data, axis=0)
+
+# Calculate correlation for every voxel 
+def calculate_dynam_corr(windows, window_length, overlap, seed_tc, data):
+    print("Calculating correlation")
+    voxel_tc = data.reshape((np.prod(data.shape[:-1]), data.shape[-1]))
+    correlation = np.zeros((data.shape[0], data.shape[1], data.shape[2], windows))
+    for window in range(windows):
+        win_lower = window * (window_length - overlap)
+        win_upper = win_lower + window_length
+        correlation[..., window] = pearson_2d(seed_tc[win_lower:win_upper], voxel_tc[:, win_lower:win_upper].T).reshape(data.shape[:-1])
+        print("window boundaries: " + str(win_lower) + " " + str(win_upper))        
+        print("seed_tc window")        
+        print(seed_tc[win_lower:win_upper])
+        print("voxel_tc window")
+        print(voxel_tc[700000, win_lower:win_upper])
+        voxel_test_tc = voxel_tc[700000, :]
+    return correlation
+    
+def calculate_static_corr(seed_tc, data):
+    print("Calculating correlation")
+    voxel_tc = data.reshape((np.prod(data.shape[:-1]), data.shape[-1]))
+    return pearson_2d(seed_tc, voxel_tc.T).reshape(data.shape[:-1])
+    
+# Calculate the z-score from correlation matrix
+def corr_to_z(correlation):
+    return np.arctanh(correlation)
+
+# Save correlation matrix as Nifti Image
+def save_output(out_matrix, header, output_name, output_dir):
+    print("Saving output matrices")
+    matrix = nib.Nifti1Image(out_matrix, header)
+    outfile_name = output_name + '.nii'
+    nib.save(matrix, outfile_name)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    shutil.move(os.path.join(os.getcwd(), outfile_name), os.path.join(output_dir, outfile_name))
+
+if __name__ == "__main__":
+    start = timer()
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("processed_epi", help="processed FMRI date (3d + time)")
+    parser.add_argument("seed_mask", help="seed mask covering ROI (3d)")
+    parser.add_argument("window_length", help="length of window in sliding-window analysis (trs)", type=int)
+    parser.add_argument("overlap", help="overlap of windows (trs)", type=int)
+    parser.add_argument("output_dir", help="output directory")
+    args = parser.parse_args()
+    
+    data, seed, header = read_data(args.processed_epi, args.seed_mask)
+    windows = calc_windows(args.window_length, args.overlap, data.shape[3])
+    seed_tc = extract_seed_tc(seed, data)
+    correlation = calculate_dynam_corr(windows, args.window_length, args.overlap, seed_tc, data)
+    correlation = calculate_static_corr(seed_tc, data)
+    z_matrix = corr_to_z(correlation)
+    output_name = os.path.basename(args.processed_epi)[:-4]
+    save_output(correlation, header, output_name + "_DMNDynamCorrMaskave", args.output_dir)
+    save_output(z_matrix, header, output_name + "_DMNDynamZMaskave", args.output_dir)
+
+    end = timer()
+    print("Time Elapsed: " + str(end - start) + " s")
--- rsfmri_pp_afni.py
+++ rsfmri_pp_afni.py
@@ -20,6 +20,8 @@ ToDo:
 	-Move files to correct folder
 	-Test EPI on MNI snapshot
 	-Create dynamic DMN QC
+	-Support blur only in gray matter
+	-Convert result to nifti and rename
 """
 
 import argparse
@@ -28,6 +30,7 @@ import subprocess
 import sys
 import shutil
 import numpy as np
+import dynamic_connectivity as dc
 from multiprocessing import cpu_count
 
 # All output is written to rsfmri_pp_afni_output.txt for verification
@@ -74,18 +77,17 @@ def parse_args(args):
 	"""
 	
 	parser = argparse.ArgumentParser(
-	    description="""Generates and runs an afni_proc.py script to preprocess 
-	                resting state fMRI data""",
+	    description="""Generates and runs an afni_proc.py script to preprocess resting state fMRI data""",
 	    formatter_class=argparse.RawDescriptionHelpFormatter)
 
 	# Optional Flags
 	parser.add_argument("-t", "--trs_remove", action="store", default=5, type=int, metavar='TRs',
-	                    help="""number of seconds of trs to remove at the beginning of the epi data
+	                    help="""number of trs to remove at the beginning of the epi data
 	                            (default = 5 trs)""")
 	parser.add_argument("-d", "--dim_voxel", action="store", default=2.0, type=float, metavar='MM',
-	                    help="voxel dimensions in mm (default = 2.0 mm)")
+	                    help="voxel dimensions in mm that processed epi will be resampled to (default = 2.0 mm)")
 	parser.add_argument("-b", "--bandpass", action="store", default=[0.01, 0.25], nargs=2, type=float, metavar="F",
-	                    help="bandpass frequencies limits (default = 0.01 0.25)")
+	                    help="bandpass frequencies lower and upper limits (default = 0.01 0.25)")
 	parser.add_argument("-v", "--volumes", action="store", default=0, type=int, metavar="V",
 						help="""truncate the epi data to the inputted number of volumes, useful if subjects have data 
 						with different numbers of volumes (default = no truncation)""")
@@ -94,19 +96,20 @@ def parse_args(args):
 	parser.add_argument("-c", "--cores", action="store", default=cpu_count(), type=int, metavar="C",
 	                    help="number of cores supplied to 3dDeconvolve (default = all cores)")
 	parser.add_argument("-s", "--subj_id", action="store", default="sub", metavar="SUB",
-	                    help="subject id (default = sub")
-	parser.add_argument("-B", "--bandpass_ts", action="store", default=0, type=float, metavar="TS",
+	                    help="subject id (default = sub)")
+	parser.add_argument("-T", "--time_step", action="store", default=0, type=float, metavar="TS",
 						help="set the time step for bandpassing (default = ts in header info")
 
 
 	parser.add_argument("-r", "--rerun", action="store_true", default=False,
-	                    help="rerun preprocessing")
+	                    help="""rerun preprocessing, override and delete previous results in 
+	                    'Processed' folder (default = don't override)""")
 	parser.add_argument("-m", "--motion_param", action="store_true", default=False,
 	                    help="use 12 motion parameters for regression (default = 6 motion parameters)")
 	parser.add_argument("-g", "--global_signal_regression", action="store_false", default=True,
 						help="perform global signal regression (default = perform gsr)")
-	# parser.add_argument("-g", "--gm_blur", action="store_true", default=False,
-	#                     help="blur only in grey matter mask (default = blur in whole brain)")
+	parser.add_argument("-G", "--gm_blur", action="store_true", default=False,
+	                     help="blur only in grey matter mask (default = blur in whole brain)")
 	parser.add_argument("-n", "--nl_reg", action="store_true", default=False,
 	                    help="use non-linear warp between anatomical and MNI template (default = linear warp)")
 
@@ -128,7 +131,7 @@ def parse_args(args):
 	assert (np.all(np.array(result.bandpass) > 0)), "Cannot have a negative frequency limit for bandpassing"
 	assert (result.volumes > -1), "Number of volumes must be greater than 0"
 	assert (result.cores > 0), "Number of cores used must be greater than 0"
-	assert (result.bandpass_ts > -1), "Time step must be greater than 0"
+	assert (result.time_step > -1), "Time step must be greater than 0"
 
 	return result
 
@@ -291,7 +294,7 @@ def generate_afni_proc(args, template):
 	afni_proc.extend([
 		"-regress_opts_3dD",
 		"-jobs", str(args.cores),
-		"-regress_run_clustersim", "no"])
+		"-regress_run_clustsim", "no"])
 
 	assert (all(isinstance(param, str) for param in afni_proc)), "Not all parameters are strings"
 	return afni_proc
@@ -331,8 +334,20 @@ def create_EM_snapshot(final_epi, subj_id, template):
 	"""
 	# Equivalent to shell command: @snapshot_volreg $template $subj.results/pb03.$subj.r01.volreg+tlrc. EM_$subj.jpg
 	# Names snapshot EM_$subj.jpg
-	record(subprocess.check_output([
-		"@snapshot_volreg", template, final_epi, "EM_{}.jpg"format(subj_id)]))
+	try:
+		record(subprocess.check_output([
+		"@snapshot_volreg", template, final_epi, "EM_{}.jpg".format(subj_id)], stderr=subprocess.STDOUT))
+	except subprocess.CalledProcessError as e:
+		record(e.output)
+		record("Error creating creating the EPI on MNI registration snapshot")
+
+def create_static_DMN(subj_id, epi, out_dir, seed):
+	data, seed, header = dc.read_data(epi, seed)
+	seed_tc = dc.extract_seed_tc(seed, data)
+	correlation = dc.calculate_static_corr(seed_tc, data)
+	output_name = os.path.basename(epi)
+	output_name = output_name[:output_name.find(".nii")]
+	dc.save_output(correlation, header, output_name + "_DMNStaticCorr", out_dir)
 
 def clean(subj_id):
 	"""
@@ -359,35 +374,39 @@ def run():
 	clean(args.subj_id)
 
 	os.environ['OMP_NUM_THREADS'] = args.cores
-	final_result = args.out_dir + create_outdir(args.out_dir)
 
-	result = "{}.results/errts.sub.tproject+tlrc".format(args.subj_id)
-	if (os.path.isfile(final_result + ".HEAD") and os.path.isfile(final_result + ".BRIK")
+	# outdir = outdir/Processed/
+	args.out_dir = create_outdir(args.out_dir)
+
+	result = args.out_dir + "{}.results/errts.{}.tproject+tlrc".format(args.subj_id, subj_id)
+	if (os.path.isfile(result + ".HEAD") and os.path.isfile(result + ".BRIK")
 		and not args.rerun):
 		record("Data has already been preprocessed.")
 		record("Results of preprocessing are the following files: ")
-		record(final_result + ".HEAD")
-		record(final_result + ".BRIK")
+		record(result + ".HEAD")
+		record(result + ".BRIK")
 		record("Use flag -r/--rerun to override results and rerun the preprocessing")
 		return
 	
 	else:
-		shutil.rmtree(args.out_dir)
+		if os.path.isdir(args.out_dir):
+			record("Overriding and deleting previous 'Processed' directory.")
+			shutil.rmtree(args.out_dir)
 		os.mkdir(args.out_dir)
 
 		if args.nl_reg:
 			run_SSwarper(args.out_dir, args.subj_id, args.anat, mni)
 		if args.volumes:
 			args.epi = truncate_epi()
-		if args.bandpass_ts:
-			set_epi_tr(args.epi, args.bandpass_ts)
+		if args.time_step:
+			set_epi_tr(args.epi, args.time_step)
 
 		afni_proc = generate_afni_proc(args, mni)
 		# Execute afni_proc.py, creates RSproc.$subj script
 		record(subprocess.check_output(afni_proc, stderr=subprocess.STDOUT))
 
 		# Execute RSproc.$subj
-		record(subprocess.check_output(["tcsh", "-xef", "RSproc.{}".format(args.subj_id)]))
+		record(subprocess.check_output(["tcsh", "-xef", "RSproc.{}".format(args.subj_id)]), stderr=subprocess.STDOUT)
 
 if __name__ == "__main__":
 	run()
